ebcda45000c173997a11866bb3c8d6c0894a19a4
tiwarikushmukesh <tiwarikushmukesh+github@gmail.com>
2024-10-16 21:04:14 +0530

    Update StoreContext.jsx

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
 
-    const url = "http://localhost:4000"
+    const url = "https://food-del-backend-odvv.onrender.com"
     const [cartItems, setCartItems] = useState({});
     const [token, setToken] = useState("")
     const currency = "₹";
